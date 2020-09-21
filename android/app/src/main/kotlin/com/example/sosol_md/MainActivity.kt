5af84bf0d9884f5bf6cc26a8b667ca1ad1ba24cd
Bagas <banabda@gmail.com>
2020-09-21 16:01:36 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/sosol_md/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sosol_md
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colorslib.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF);
+
+const kDefaultPadding = 20.0;
+
+final kDefaultShadow = BoxShadow(
+    blurRadius: 10, offset: Offset(5, 5), color: Color(0xFF).withOpacity(0.56));
